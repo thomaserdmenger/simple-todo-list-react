ff6d108f6b2c1e2377d9e4d317883ca84d45ff9d
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-11-20 06:38:00 +0100

    Add basic structure for todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,33 @@
+import * as React from "react";
+
 export default function App() {
-  return null;
+  const [todos, setTodos] = React.useState([
+    { id: crypto.randomUUID(), name: "Homework" },
+    { id: crypto.randomUUID(), name: "Kitchenwork" },
+  ]);
+
+  return (
+    <main>
+      <div>
+        {todos != null &&
+          todos.map((todo) => {
+            return (
+              <div key={todo.id}>
+                <input type="checkbox" />
+                {todo.name}
+                <button>Delete</button>
+              </div>
+            );
+          })}
+      </div>
+      <br />
+      <form>
+        <label>Todo List</label>
+        <br />
+        <input type="text" placeholder="Enter todo here" />
+        <br />
+        <button type="submit">Add Todo</button>
+      </form>
+    </main>
+  );
 }
